e7e4baa6764a708202e19015c673ad2bfa582145
GabbeRique <159442633+GabbeRique@users.noreply.github.com>
2024-08-27 20:28:36 -0300

    Add files via upload

---
+++ DD/projeto_cad_usuario/app_cad_usuario/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Usuarios(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=30)
+    numero = models.TextField(max_length=13)
+    senha = models.TextField(max_length=99)
